5eda85720553507e4f25d24417d784cb6d016441
b-ellis <bradjellis18@gmail.com>
2017-01-04 09:49:32 -0500

    get / added

--- js/server.js
+++ js/server.js
@@ -27,6 +27,9 @@ const createStorage = function(){
 
 const storage = createStorage();
 
+app.get('/', (req, res) {
+	return res.body;
+});
 
 app.get('/fewest', function(req, res) {
 	return res.json({fewest: storage.fewest});
